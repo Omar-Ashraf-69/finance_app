67e0c4dc1b8006deff624ec9bc92df1925baf201
OmarAsharf62 <omarbondok62@gmail.com>
2024-03-11 21:00:35 +0200

    added the shared preference to control showing the onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
-import 'package:finanice_app/screens/on_boarding_screen.dart';
-import 'package:finanice_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:finanice_app/screens/splash_screen.dart';
 
-void main() {
+void main()  {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const OnBoardingScreen(),
+      home: const SplashScreen(),
     );
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,8 +2,10 @@ import 'dart:async';
 
 import 'package:finanice_app/colors/colors.dart';
 import 'package:finanice_app/screens/home_screen.dart';
+import 'package:finanice_app/screens/on_boarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -16,12 +18,24 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 2), () {
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const HomeScreen(),
-          ));
+
+    _checkFirstSeen();
+  }
+
+  void _checkFirstSeen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool seen = (prefs.getBool('seenOnboarding') ?? false);
+
+    Timer(const Duration(seconds: 2), () async  {
+      if (seen == true) {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const HomeScreen()));
+    } else {
+      await prefs.setBool('seenOnboarding', true);
+      // ignore: use_build_context_synchronously
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const OnBoardingScreen()));
+    }
     });
   }
 
